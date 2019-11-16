badd38fcbac09eb1829b18d487e236355b5097e7
imamachi-n <naoto.imamachi@gmail.com>
2019-11-16 17:32:23 +0900

    Created voice control sample

---
+++ HoloLens-MRTKv2-Voice-Control/Assets/Scenes/VoiceControlsample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceControlsample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void test()
+    {
+        Debug.Log("OK!!");
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
